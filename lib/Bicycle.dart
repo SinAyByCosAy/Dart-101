7d013611e8729ce23e9c5c749f8a45e97838a91b
TanaySrivas <tanaysriofficial@gmail.com>
2023-03-15 04:33:26 +0530

    Adding custom string representation

--- lib/Bicycle.dart
+++ lib/Bicycle.dart
@@ -2,10 +2,13 @@ class Bicycle{
   int cadence;
   int speed;
   int gear;
-  Bicycle(this.cadence, this.speed, this.gear);
+  Bicycle(this.cadence, this.speed, this.gear);//equivalent to a proper constuctor
+
+  @override
+  String toString() => "Bicycle: $speed mph"; //for custom string representation of the class object
 }
 
 void main(){
   var bike = Bicycle(2, 0, 1);
-  print(bike);
+  print(bike);//getting the instace of the class 
 }
